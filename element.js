8b8a8b7c31ba59ac6213d257d515b64cd095e619
unexpectedprojectz <alexjimenezcamacho76@gmail.com>
2020-12-02 20:02:46 +0100

    Creació de constructors

---
+++ element.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+
+    var Element = function(nom) {
+        this.nom = nom;
+        //this.caminar = function(dist)
+        //{ console.log(this.nom + ’ camina ’ + dist + ’ m.’);
+        //}
+    };
+
+}
--- js/main.js
+++ js/main.js
@@ -3,12 +3,19 @@ window.onload = function() {
     let repetir = true;
     let numero;
 
-    while(repetir == true) {
+    while (repetir == true) {
         numero = prompt("Introdueix un número de files i columnes contingut entre 5 i 20");
 
         if (numero >= 5 && numero <= 20) {
             repetir = false;
         }
     }
-    
+
+    var Tauler = {
+        elements: []
+        //dades: function() // metodes : funció,
+        //{ return(this.nom + "-" + this.ocupacio + "-" + this.edat);
+        //}
+    };
+       
 }
