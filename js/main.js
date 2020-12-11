7946f63817a2f2f7231f0ac1a6a6f8a626c05dab
unexpectedprojectz <alexjimenezcamacho76@gmail.com>
2020-12-11 18:29:24 +0100

    Variables inicials

--- js/main.js
+++ js/main.js
@@ -3,7 +3,12 @@ window.onload = function() {
 
 
     let repetir = true;
-    let numero;
+    let numero, caselles, estrelles, zombis, doblarpuntacio, eliminarzombis, vidaextra;
+
+    caselles = numero * numero;
+    estrelles = numero;
+    zombis = Math.round((caselles * 25) / 100);
+    doblarpuntuacio = 1, eliminarzombis = 2, vidaextra = 3; // PARA ESTOS TRES HABRÁ QUE CREAR ATRIBUTO BOOLEAN POSICIO PARA SABER SI ES VERTICAL 0 O HORIZONTAL 1 CUANDO SE COLOQUE EN TABLERO
 
     while (repetir == true) {
         numero = prompt("Introdueix un número de files i columnes contingut entre 5 i 20");
