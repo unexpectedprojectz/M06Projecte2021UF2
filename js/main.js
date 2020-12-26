8142473bc3e734b875d27084cde6ff2f18f0129a
alexartes <IH8zzeDf>
2020-12-26 13:38:38 +0100

    Proves de localStorage

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ function creacioTauler() {
 
     //si el valor és correcte (entre 5 i 20) podem crear el tauler
     if (valor >= 5 && valor <= 20) {
-        
+
         //inicialitzem els elements i preparem els que necessitarem de cada tipus
         let totalcaselles, estrelles, zombis, doblarpuntuacions, meitatzombis, videsextres;
         let rcolumna, rfila, seguir;
@@ -39,17 +39,17 @@ function creacioTauler() {
 
         totalcaselles = valor * valor;
         zombis = Math.round((totalcaselles * 25) / 100);
-    
+
         doblarpuntuacions = 1, meitatzombis = 2, videsextres = 3;
 
         //creem l'objecte tauler
         var Tauler = {
-            
+
             casellesusuari: [],
             casellesresposta: [],
 
             inicialitzador: function (filesColumnes) {
-                
+
                 //TAULER DE CARA A L'USUARI
                 //creem les posicions del tauler que veurà l'usuari segons numero de files i columnes
                 for (var files = 0; files <= filesColumnes - 1; files++) {
@@ -61,7 +61,7 @@ function creacioTauler() {
                         this.casellesusuari[files][columnes] = new Element('g');
                     }
                 }
-                
+
                 // TAULER RESPOSTA
                 //creem les posicions del tauler resposta segons numero de files i columnes
                 for (var files = 0; files <= filesColumnes - 1; files++) {
@@ -85,11 +85,11 @@ function creacioTauler() {
 
                         do {
                             rfila = Math.floor(Math.random() * (valor));
-                            if (this.casellesresposta[rcolumna][rfila].contingut == 'g'){ //si la casella es troba lliure
+                            if (this.casellesresposta[rcolumna][rfila].contingut == 'g') { //si la casella es troba lliure
                                 this.casellesresposta[rcolumna][rfila] = new VidaExtra('v');
                                 seguir = false;
                             }
-                        } while(seguir==true);
+                        } while (seguir == true);
                     }
                 }
                 else { //si és horitzontal
@@ -99,11 +99,11 @@ function creacioTauler() {
 
                         do {
                             rcolumna = Math.floor(Math.random() * (valor));
-                            if (this.casellesresposta[rcolumna][rfila].contingut == 'g'){ //si la casella es troba lliure
+                            if (this.casellesresposta[rcolumna][rfila].contingut == 'g') { //si la casella es troba lliure
                                 this.casellesresposta[rcolumna][rfila] = new VidaExtra('v');
                                 seguir = false;
                             }
-                        } while(seguir==true);
+                        } while (seguir == true);
                     }
                 }
 
@@ -116,11 +116,11 @@ function creacioTauler() {
 
                         do {
                             rfila = Math.floor(Math.random() * (valor));
-                            if (this.casellesresposta[rcolumna][rfila].contingut == 'g'){ //si la casella es troba lliure
+                            if (this.casellesresposta[rcolumna][rfila].contingut == 'g') { //si la casella es troba lliure
                                 this.casellesresposta[rcolumna][rfila] = new MeitatZombis('m');
                                 seguir = false;
                             }
-                        } while(seguir==true);
+                        } while (seguir == true);
                     }
                 }
                 else { //si és horitzontal
@@ -130,25 +130,25 @@ function creacioTauler() {
 
                         do {
                             rcolumna = Math.floor(Math.random() * (valor));
-                            if (this.casellesresposta[rcolumna][rfila].contingut == 'g'){ //si la casella es troba lliure
+                            if (this.casellesresposta[rcolumna][rfila].contingut == 'g') { //si la casella es troba lliure
                                 this.casellesresposta[rcolumna][rfila] = new MeitatZombis('m');
                                 seguir = false;
                             }
-                        } while(seguir==true);
+                        } while (seguir == true);
                     }
                 }
 
                 //doble puntuacio
                 seguir = true;
-                
+
                 do {
                     rcolumna = Math.floor(Math.random() * (valor));
                     rfila = Math.floor(Math.random() * (valor));
-                    if (this.casellesresposta[rcolumna][rfila].contingut == 'g'){ //si la casella es troba lliure
+                    if (this.casellesresposta[rcolumna][rfila].contingut == 'g') { //si la casella es troba lliure
                         this.casellesresposta[rcolumna][rfila] = new DoblePunts('d');
                         seguir = false;
                     }
-                } while(seguir==true);
+                } while (seguir == true);
 
                 //omplim amb recompenses aleatories fins el 25% del tauler
                 let totalrecompensesinicials = doblarpuntuacions + meitatzombis + videsextres;
@@ -156,12 +156,12 @@ function creacioTauler() {
                 for (var contrestarecompenses = 1; contrestarecompenses <= restarecompenses; contrestarecompenses++) {
                     tipusrecompensa = Math.round(Math.random() * 2); // 0 = vida extra, 1 = meitat zombis, 2 = doble puntuacio
                     seguir = true;
-                
+
                     do {
                         rcolumna = Math.floor(Math.random() * (valor));
                         rfila = Math.floor(Math.random() * (valor));
-                        if (this.casellesresposta[rcolumna][rfila].contingut == 'g'){ //si la casella es troba lliure
-                            switch(tipusrecompensa){
+                        if (this.casellesresposta[rcolumna][rfila].contingut == 'g') { //si la casella es troba lliure
+                            switch (tipusrecompensa) {
                                 case 0: //vida extra
                                     this.casellesresposta[rcolumna][rfila] = new VidaExtra('v');
                                     videsextres++;
@@ -173,64 +173,64 @@ function creacioTauler() {
                                 case 2: //doble puntuacio
                                     this.casellesresposta[rcolumna][rfila] = new DoblePunts('d');
                                     doblarpuntuacions++;
-                                break;
+                                    break;
                             }
                             seguir = false;
                         }
-                    } while(seguir==true);
+                    } while (seguir == true);
                 }
 
                 //ESTRELLES
                 for (var contestrelles = 1; contestrelles <= estrelles; contestrelles++) {
                     seguir = true;
-                
+
                     do {
                         rcolumna = Math.floor(Math.random() * (valor));
                         rfila = Math.floor(Math.random() * (valor));
-                        if (this.casellesresposta[rcolumna][rfila].contingut == 'g'){ //si la casella es troba lliure
+                        if (this.casellesresposta[rcolumna][rfila].contingut == 'g') { //si la casella es troba lliure
                             this.casellesresposta[rcolumna][rfila] = new Estrella('e');
                             seguir = false;
                         }
-                    } while(seguir==true);
+                    } while (seguir == true);
                 }
 
                 //ZOMBIS
                 for (var contzombis = 1; contzombis <= zombis; contzombis++) {
                     seguir = true;
-                
+
                     do {
                         rcolumna = Math.floor(Math.random() * (valor));
                         rfila = Math.floor(Math.random() * (valor));
-                        if (this.casellesresposta[rcolumna][rfila].contingut == 'g'){ //si la casella es troba lliure
+                        if (this.casellesresposta[rcolumna][rfila].contingut == 'g') { //si la casella es troba lliure
                             this.casellesresposta[rcolumna][rfila] = new Zombi('z');
                             seguir = false;
                         }
-                    } while(seguir==true);
+                    } while (seguir == true);
                 }
             },
 
             veureContingut: function () {
                 for (var files = 0; files <= valor - 1; files++) {
                     for (var columnes = 0; columnes <= valor - 1; columnes++) {
-                        if(this.casellesusuari[files][columnes].contingut == 'g') {
+                        if (this.casellesusuari[files][columnes].contingut == 'g') {
                             imatge = "img/gespa.jpg"
                         }
-                        if(this.casellesusuari[files][columnes].contingut == 'z') {
+                        if (this.casellesusuari[files][columnes].contingut == 'z') {
                             imatge = "img/zombi.jpg"
                         }
-                        if(this.casellesusuari[files][columnes].contingut == 'e') {
+                        if (this.casellesusuari[files][columnes].contingut == 'e') {
                             imatge = "img/estrella.jpg"
                         }
-                        if(this.casellesusuari[files][columnes].contingut == 'd') {
+                        if (this.casellesusuari[files][columnes].contingut == 'd') {
                             imatge = "img/doblepuntuacio.jpg"
                         }
-                        if(this.casellesusuari[files][columnes].contingut == 'm') {
+                        if (this.casellesusuari[files][columnes].contingut == 'm') {
                             imatge = "img/meitatzombis.jpg"
                         }
-                        if(this.casellesusuari[files][columnes].contingut == 'v') {
+                        if (this.casellesusuari[files][columnes].contingut == 'v') {
                             imatge = "img/vidaextra.jpg"
                         }
-                        
+
                         textTauler.innerHTML += "<button id='" + files + "-" + columnes + "' onclick='(function(){Tauler.canviarContingut()})'><img src='" + imatge + "'></button>";
                         /*document.getElementById("0-0").onclick = function() {
                             canviarContingut();
@@ -262,3 +262,27 @@ function creacioTauler() {
 
 
 }
+
+/* ********************** localStorage *********************************** */
+
+/* emmagatzema dades sense data de caducitat.
+ Les dades no es suprimiran quan el navegador estigui tancat i estaran disponibles el dia,
+ la setmana o l'any següent. */
+
+/* GUARDAR DATO
+
+localStorage.setItem("key", "value");
+
+*/
+
+/* LEER DATO
+
+var lastname = localStorage.getItem("key");
+
+*/
+
+/* ELIMINAR DATO
+
+localStorage.removeItem("key");
+
+*/
